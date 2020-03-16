d226a9f8580a50a7d53b3f7c9c115ca792dd97e4
Daniel Sun <danielsun12345@gmail.com>
2020-03-16 22:20:40 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Brinch", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Male", "Brown", "Shy", false)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "Male", "Brown", "Shy", false)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 1, "Male", "Brown", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "Male", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Male", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "Male", "Orange", "Shy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "Blue", "Shy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Brinch", 6, "Female", "Red", "Aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "Female", "Turqoise", "Shy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "Female", "Yellow", "Aggressive", 0);
